79507a43e96735ecbbfdb8af7951f2b2076d0bdf
3st4d <marco.anchiraico@gmail.com>
2021-09-16 02:38:50 -0500

    Uso de logger en lugar de println en Prueba

--- src/main/java/cs/ing/software/lab1/Prueba.java
+++ src/main/java/cs/ing/software/lab1/Prueba.java
@@ -1,6 +1,10 @@
 package cs.ing.software.lab1;
 
+import java.util.logging.Logger;
+
 public class Prueba {
+
+    static final Logger logger = Logger.getLogger(Prueba.class.getName());
 
     static double consumoAutomovil = 0.9;
     static double consumoCamion = 1.6;
@@ -9,9 +13,9 @@ public class Prueba {
     public static void main(String[] args) {
         double cantidad = 10;
         Automovil hyundai = new Automovil(cantidad, consumoAutomovil, capacidadVehiculo);
-        System.out.println(hyundai.conducir(12));
+        logger.info(hyundai.conducir(12));
         Camion mitsubishi = new Camion(cantidad, consumoCamion, capacidadVehiculo);
-        System.out.println(mitsubishi.conducir(4));
+        logger.info(mitsubishi.conducir(4));
     }
 
 }
