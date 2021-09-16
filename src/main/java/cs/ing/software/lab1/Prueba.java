8e0a9149e01170eacb0991a705094f317c06df97
3st4d <marco.anchiraico@gmail.com>
2021-09-16 03:33:53 -0500

    Solo quedarme con una prueba en el main

--- src/main/java/cs/ing/software/lab1/Prueba.java
+++ src/main/java/cs/ing/software/lab1/Prueba.java
@@ -19,12 +19,6 @@ public class Prueba {
         Automovil hyundai = new Automovil(cantidad, consumoAutomovil, capacidadVehiculo);
         String hyundaiConducir = hyundai.conducir(12);
         logger.info(hyundaiConducir);
-        Camion mitsubishi = new Camion(cantidad, consumoCamion, capacidadVehiculo);
-        String mitsubishiConducir = mitsubishi.conducir(4);
-        logger.info(mitsubishiConducir);
-        Vehiculo vehiculo1 = new Vehiculo(cantidad, consumoCamion, capacidadVehiculo);
-        String vehiculoRecargar = vehiculo1.recargar(4);
-        logger.info(vehiculoRecargar);
     }
 
 }
