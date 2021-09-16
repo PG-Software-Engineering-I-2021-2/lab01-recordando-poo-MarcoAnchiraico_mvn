26e5ee4bbdb70893313a03a29e68b4d0942c544a
3st4d <marco.anchiraico@gmail.com>
2021-09-16 02:19:25 -0500

    Se agrego 2da prueba unitaria

--- src/test/java/cs/ing/software/lab1/PruebaTest.java
+++ src/test/java/cs/ing/software/lab1/PruebaTest.java
@@ -10,5 +10,8 @@ public class PruebaTest {
     public void Test01(){
         Automovil audi = new Automovil(10, consumoAutomovil, capacidadVehiculo);
         Assert.assertEquals(audi.recargar(500),"Automovil no se puede reabastecer el tanque, esta lleno");
+
+        Camion volvo = new Camion(10, consumoAutomovil, capacidadVehiculo);
+        Assert.assertEquals(volvo.conducir(4),"Camion viajo 4.0 km y aun tiene 6.40 de combustible");
     }
 }
